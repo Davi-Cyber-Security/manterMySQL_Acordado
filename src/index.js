48b8f29661ccce8ecb6af90a24ebd0712b4bab1c
Davi Cyber Security <davigabriell442@gmail.com>
2025-09-30 18:13:13 -0300

    Refatorado código seguindo boas práticas do Clean Code Engineer Coding

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import { manterConexao } from './service/servico.js';
 import router from './routes/rotas.routes.js';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use('/', router);
 
 setInterval(manterConexao, 1000 * 60 * 5);
 manterConexao();
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Servidor rodando na porta ${process.env.PORT || 3000}`);
+
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
